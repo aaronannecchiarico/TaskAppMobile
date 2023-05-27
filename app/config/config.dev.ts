589a00e1bd23881fdcc7e0862aa92ec65420cea5
aannecchiarico <aannecchiarico@protonmail.com>
2023-05-27 00:09:58 -0400

    remove todo

--- app/config/config.dev.ts
+++ app/config/config.dev.ts
@@ -6,6 +6,5 @@
  * https://reactnative.dev/docs/security#storing-sensitive-info
  */
 export default {
-  // TODO: sort out why this is necessary for android
   API_URL: "http://cxh1-b.tsetc.net:3000/api/v1",
 }
